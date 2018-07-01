b3e5aa2c95eb8be0d0e0e6ad8e9050b5a55570f7
hyendler <hyendler@gmail.com>
2018-07-01 09:03:23 -0700

    Add instructions for form image uploading

--- src/components/ImageUploaderForm.jsx
+++ src/components/ImageUploaderForm.jsx
@@ -14,4 +14,18 @@
 	 // write a handleChange() function to store the input
 	 // write a handleSubmit() function to print to the console
 
+// Exercise 6: BONUS
+// Goal: make this form actually upload an image to the feed!
+// When a user inputs a full image url, it should update the
+// feed to include the image as a FeedItem!
+    // All the data for the photos are located in HomePage -
+    // so how do you get data from the user input of the ImageUploaderForm
+    // to the HomePage component and update the photo data?
+    // If photos data on the homepage is changing, then you will need to set it state.
+    // And then, in Homepage, you will need to create a function called
+    // handleAddPhoto that updates photos state by accepting a url and creating
+    // a new object in photos data.
+    // Then pass in that function as props to ImageUploaderForm
+    // and when the user submits, call that function with the user submitted url.
+
 import React from 'react';

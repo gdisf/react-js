15baec118d4273397fddf4e0aba287b7065ecab9
Nicole Fish <fish@elevatesecurity.com>
2018-07-01 02:10:03 -0700

    add exercise 6 instructions to form component

--- src/components/ImageUploaderForm.jsx
+++ src/components/ImageUploaderForm.jsx
@@ -1,7 +1,17 @@
 // Exercise 2: Set It Up
 	// write a functional component
 	// make it a form element with class name ImageUploaderForm
-	// write one input element and button element inside.
+	// write one input element and button element inside
 	// export the component!
+
+// Exercise 6: Get InFormation
+// Rewrite this component to accept user input
+// When the user inputs a url, the component should log it to the console
+	 // create internal state, to hold the user inputted value
+	 // render a form element containing:
+			// input element with a onChange attribute
+			// button with an onSubmit() attribute
+	 // write a handleChange() function to store the input
+	 // write a handleSubmit() function to print to the console
 
 import React from 'react';
